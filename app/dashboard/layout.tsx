16ef1b2c041b6726004d2a4b54dfd84fc8616812
designindev <2438599@gmail.com>
2024-12-23 18:15:27 +0200

    changed layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,9 +7,9 @@ const DashboardLayout = ({
   children: React.ReactNode
 }>) => {
   return (
-    <div className='container flex min-h-screen gap-4'>
+    <div className='container flex min-h-screen gap-5'>
       <Sidebar />
-      <main>
+      <main className='grow'>
         <Header />
         {children}
       </main>

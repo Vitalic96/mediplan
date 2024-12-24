bf2de50bd35cb2c1cb982c9d8d4b303caef29b20
designindev <2438599@gmail.com>
2024-12-24 13:18:13 +0200

    added mock data

--- lib/mocks.ts
+++ lib/mocks.ts
@@ -1,14 +1,48 @@
-import Image1 from '@/assets/images/dashboard/1-1.jpg'
-import Image2 from '@/assets/images/dashboard/1-2.jpg'
-import Image3 from '@/assets/images/dashboard/1-3.jpg'
-import Image4 from '@/assets/images/dashboard/1-4.jpg'
-import Image5 from '@/assets/images/dashboard/1-5.jpg'
+import Image1_1 from '@/assets/images/dashboard/1-1.jpg'
+import Image1_2 from '@/assets/images/dashboard/1-2.jpg'
+import Image1_3 from '@/assets/images/dashboard/1-3.jpg'
+import Image1_4 from '@/assets/images/dashboard/1-4.jpg'
+import Image1_5 from '@/assets/images/dashboard/1-5.jpg'
+import Image2_1 from '@/assets/images/dashboard/2-1.jpg'
+import Image2_2 from '@/assets/images/dashboard/2-2.jpg'
+import Image2_3 from '@/assets/images/dashboard/2-3.jpg'
+import Image2_4 from '@/assets/images/dashboard/2-4.jpg'
+import Image3_1 from '@/assets/images/dashboard/3-1.jpg'
+import Image3_2 from '@/assets/images/dashboard/3-2.jpg'
+import Image3_3 from '@/assets/images/dashboard/3-3.jpg'
+import Image3_4 from '@/assets/images/dashboard/3-4.jpg'
 
-export const mockListActivity = [Image1, Image2, Image3, Image4, Image5].map(
-  (image) => ({
-    image,
-    name: 'Lorem Ipsum',
-    info: 'Lorem Ipsum',
-    date: '03.04.2021',
-  }),
-)
+export const mockListActivity = [
+  Image1_1,
+  Image1_2,
+  Image1_3,
+  Image1_4,
+  Image1_5,
+].map(
+  (image) =>
+    ({
+      image,
+      name: 'Lorem Ipsum',
+      info: 'Lorem Ipsum',
+      date: '03.04.2021',
+    }) satisfies UserCard,
+) as UserCard[]
+
+export const mockSearchList = [Image2_1, Image2_2, Image2_3, Image2_4].map(
+  (image) =>
+    ({
+      image,
+      name: 'Lorem Ipsum',
+    }) satisfies UserCard,
+) as UserCard[]
+
+export const mockCommunityList = [Image3_1, Image3_2, Image3_3, Image3_4].map(
+  (image) =>
+    ({
+      image,
+      name: 'Lorem Ipsum',
+      description: 'Lorem IpsumLorem IpsumLorem IpsumLorem Ipsum',
+      info: 'Lorem Ipsum',
+      date: '03.04.2021',
+    }) satisfies UserCard,
+) as UserCard[]

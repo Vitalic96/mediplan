3126a8c3aab6023268984a83d4e3d150879dd8c3
designindev <2438599@gmail.com>
2024-12-24 11:14:29 +0200

    changed home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import Link from 'next/link'
+
+export default function HomePage() {
   return (
-    <div className='font-poppits flex min-h-screen flex-col items-center justify-center gap-16 bg-green-500 p-8 pb-20'>
-      <h1 className='text-3xl'>Home page</h1>
+    <div className='flex min-h-screen flex-col items-center justify-center gap-8 p-8 pb-20'>
+      <h1 className='text-5xl'>Home page</h1>
+      <Link
+        className='inline-flex h-10 items-center justify-center rounded-xl bg-violet-400 px-10 py-1 font-medium text-white transition-colors hover:bg-violet-500 active:bg-violet-600'
+        href='/dashboard'
+      >
+        Go to dashboard
+      </Link>
     </div>
   )
 }

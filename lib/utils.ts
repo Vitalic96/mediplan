82c83c367b1761179d79bc00a18a542a6d1ac775
designindev <2438599@gmail.com>
2024-12-25 10:18:54 +0200

    added get date format

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
+export const getDateFormat = (date: Date) => {
+  return `${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`
+}

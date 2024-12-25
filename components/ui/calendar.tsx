49ffbbc2b8cfd64e4cef08ce448a2d38042022f9
designindev <2438599@gmail.com>
2024-12-25 10:17:24 +0200

    changed styles

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -21,24 +21,25 @@ function Calendar({
       showOutsideDays={showOutsideDays}
       className={cn('border-none', className)}
       classNames={{
-        months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
-        month: 'space-y-4 w-full',
-        caption: 'flex justify-center pt-1 relative items-center',
+        months: 'flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0',
+        month: 'flex w-full flex-col gap-4',
+        caption:
+          'relative flex min-h-8 items-center justify-center pl-10 pr-10',
         caption_label: 'font-normal text-gray-700',
-        nav: 'space-x-1 flex items-center',
+        nav: 'absolute bottom-0 left-0 right-0 top-0 flex items-center justify-between space-x-1',
         nav_button: cn(
           buttonVariants({ variant: 'ghost' }),
-          'h-6 w-6 bg-transparent p-0 opacity-50 hover:opacity-100',
+          'h-8 w-8 bg-transparent p-0 opacity-50 hover:opacity-100',
         ),
-        nav_button_previous: 'absolute left-1',
-        nav_button_next: 'absolute right-1',
-        table: 'w-full border-collapse space-y-1 ',
+        nav_button_previous: '',
+        nav_button_next: '',
+        table: 'w-full border-collapse space-y-1',
         head_row: 'flex',
         head_cell:
-          'text-gray-500/40 rounded-md w-6 flex-1 font-normal text-[0.8rem] dark:text-neutral-400',
-        row: 'flex w-full mt-2',
+          'w-6 flex-1 rounded-md text-[0.8rem] font-normal text-gray-500/40 dark:text-neutral-400',
+        row: 'flex h-7 w-full items-center',
         cell: cn(
-          't relative flex-1 p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-transparent [&:has([aria-selected].day-outside)]:bg-neutral-100/50 [&:has([aria-selected].day-range-end)]:rounded-r-md',
+          'relative flex-1 p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-transparent [&:has([aria-selected].day-outside)]:bg-neutral-100/50 [&:has([aria-selected].day-range-end)]:rounded-r-md',
           props.mode === 'range'
             ? '[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md'
             : '[&:has([aria-selected])]:rounded-md',

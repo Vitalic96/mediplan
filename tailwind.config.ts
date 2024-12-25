677c85c7cd2cfbbe643f5c55d895e012b3178923
designindev <2438599@gmail.com>
2024-12-25 17:38:38 +0200

    added screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ export default {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      '2xs': '390px',
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1340px',
+    },
     container: {
       center: true,
       padding: {

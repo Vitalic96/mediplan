27f6cf228aaa67d12f1e5b75ab2486226ae9d92c
designindev <2438599@gmail.com>
2024-12-26 17:47:23 +0200

    moved patient actions

---
+++ app/dashboard/members/_components/patient-actions.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import { ChangeEventHandler, forwardRef, useState } from 'react'
+import { Bell, Ellipsis, Trash2, UserRound } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import TelegramIcon from '@/assets/icons/telegram.svg'
+
+const PatientActions = () => {
+  const [image, setImage] = useState('')
+
+  const handleLoadImage: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const [file] = event.target.files ?? []
+
+    if (file == null) {
+      return
+    }
+
+    const fileReader = new FileReader()
+
+    fileReader.addEventListener('load', (event) => {
+      if (typeof fileReader.result === 'string') {
+        setImage(fileReader.result)
+      }
+    })
+
+    fileReader.readAsDataURL(file)
+  }
+
+  const handleDelete = () => {
+    // delete logic
+  }
+
+  return (
+    <div className='flex items-center gap-5'>
+      <label className='relative flex aspect-square w-[100px] shrink-0 cursor-pointer items-center justify-center rounded-full bg-gray-100 text-white xl:w-[150px]'>
+        <UserRound size={'50%'} />
+        {image && (
+          <img
+            src={image}
+            alt=''
+            className='absolute bottom-0 left-0 right-0 top-0 h-full w-full object-cover'
+          />
+        )}
+        <input type='file' onChange={handleLoadImage} className='hidden' />
+      </label>
+      <div className='flex grow flex-col gap-2 xl:flex-row'>
+        <div className='grow'>
+          <div className='text-xl leading-[1.3] 2xs:text-2xl xl:max-w-[250px] xl:text-[28px]'>
+            Lorem Ipsum
+          </div>
+          <Button
+            variant='link'
+            className='hidden text-base text-[#FF5454] xl:inline-flex'
+            onClick={handleDelete}
+          >
+            <Trash2 />
+            Delete Pacient
+          </Button>
+        </div>
+        <ul className='flex shrink-0 gap-3 2xs:gap-5 xl:pt-1'>
+          <li>
+            <Button size='xsIcon' className='xl:w-6'>
+              <TelegramIcon className='w-[16px]' />
+            </Button>
+          </li>
+          <li>
+            <Button size='xsIcon' className='xl:w-6'>
+              <Ellipsis size={16} />
+            </Button>
+          </li>
+          <li>
+            <Button size='xsIcon' className='xl:w-6'>
+              <Bell fill='white' size={12} />
+            </Button>
+          </li>
+          <li className='xl:hidden'>
+            <Button
+              size='xsIcon'
+              variant='ghost'
+              className='bg-gray-600 text-white xl:w-6'
+            >
+              <Trash2 size={14} />
+            </Button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export { PatientActions }
--- app/dashboard/members/_components/patient-widget.tsx
+++ app/dashboard/members/_components/patient-widget.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { forwardRef, useState } from 'react'
-import { Bell, Ellipsis, Trash2, UserRound } from 'lucide-react'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
@@ -9,7 +8,7 @@ import { cn } from '@/lib/utils'
 import { patientTabList } from '@/constants'
 import { BasicInfoTab } from './tabs/basic-info-tab'
 import { EVisitHistoryTab } from './tabs/e-visit-history-tab'
-import TelegramIcon from '@/assets/icons/telegram.svg'
+import { PatientActions } from './patient-actions'
 
 const PatientWidget = forwardRef<
   HTMLDivElement,
@@ -18,57 +17,14 @@ const PatientWidget = forwardRef<
   const [activeTab, setActiveTab] = useState<(typeof patientTabList)[number]>(
     patientTabList[0],
   )
+
   return (
     <Card ref={ref} className={cn('lg:max-h-[850px]', className)} {...props}>
       <CardHeader className='shrink-0 gap-y-8'>
         <CardTitle variant='lg'>A patient</CardTitle>
       </CardHeader>
       <CardContent className='custom-scroll flex flex-col gap-4 overflow-x-hidden lg:gap-10 lg:pt-5'>
-        <div className='flex items-center gap-5'>
-          <div className='flex aspect-square w-[100px] shrink-0 items-center justify-center rounded-full bg-gray-100 text-white xl:w-[150px]'>
-            <UserRound size={'50%'} />
-          </div>
-          <div className='flex grow flex-col gap-2 xl:flex-row'>
-            <div className='grow'>
-              <div className='text-xl leading-[1.3] 2xs:text-2xl xl:max-w-[250px] xl:text-[28px]'>
-                Lorem Ipsum
-              </div>
-              <Button
-                variant='link'
-                className='hidden text-base text-[#FF5454] xl:inline-flex'
-              >
-                <Trash2 />
-                Delete Pacient
-              </Button>
-            </div>
-            <ul className='flex shrink-0 gap-3 2xs:gap-5 xl:pt-1'>
-              <li>
-                <Button size='xsIcon' className='xl:w-6'>
-                  <TelegramIcon className='w-[16px]' />
-                </Button>
-              </li>
-              <li>
-                <Button size='xsIcon' className='xl:w-6'>
-                  <Ellipsis size={16} />
-                </Button>
-              </li>
-              <li>
-                <Button size='xsIcon' className='xl:w-6'>
-                  <Bell fill='white' size={12} />
-                </Button>
-              </li>
-              <li className='xl:hidden'>
-                <Button
-                  size='xsIcon'
-                  variant='ghost'
-                  className='bg-gray-600 text-white xl:w-6'
-                >
-                  <Trash2 size={14} />
-                </Button>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <PatientActions />
         <div className='flex grow flex-wrap xs:gap-2 lg:-mx-4 xl:-ml-3 xl:-mr-8 xl:gap-x-1 xl:gap-y-[10px] xl:pt-2'>
           {patientTabList.map((label, i) => {
             const isActive = activeTab === label

1609037807cd94e53e4ad222019ee1522a1acc6d
designindev <2438599@gmail.com>
2024-12-26 12:16:33 +0200

    refactored calendar widget

--- app/dashboard/_components/calendar-widget.tsx
+++ app/dashboard/_components/calendar-widget.tsx
@@ -1,65 +1,45 @@
 'use client'
 
-import { useMemo, useState } from 'react'
+import { forwardRef, useState } from 'react'
 
-import { Calendar } from '@/components/ui/calendar'
-import { SelectSingleEventHandler } from 'react-day-picker'
-import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { DayTab } from './day-tab'
+import { MonthTab } from './month-tab'
 
-const monthNames = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-]
-
-const getFormatDate = (date: Date) => {
-  return `${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`
-}
-
-const CalendarWidget = () => {
-  const [date, setDate] = useState<Date | undefined>(new Date())
-  const [firstDayDate, lastDayDate] = useMemo(() => {
-    if (date == undefined) {
-      return [new Date(), new Date()]
-    }
-    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1)
-    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0)
-
-    return [firstDay, lastDay]
-  }, [date])
-
-  const handleSelect: SelectSingleEventHandler = (date) => {
-    setDate(date)
-  }
+const CalendarWidget = forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>((props, ref) => {
+  const [activeIndexTab, setActiveIndexTab] = useState(0)
 
   return (
-    <div className='flex flex-col gap-8 pl-5'>
-      <div className='flex items-center gap-2'>
-        <Badge className='flex-1' variant='gray'>
-          {getFormatDate(firstDayDate)}
-        </Badge>
-        <div className='h-0.5 w-3 shrink-0 rounded-full bg-gray-500'></div>
-        <Badge className='flex-1' variant='gray'>
-          {getFormatDate(lastDayDate)}
-        </Badge>
-      </div>
-      <Calendar
-        mode='single'
-        selected={date}
-        onSelect={handleSelect}
-        className='rounded-md border'
-      />
-    </div>
+    <Card ref={ref} {...props}>
+      <CardHeader className='min-h-[90px] shrink-0 gap-x-6 gap-y-3 xl:flex-row xl:items-center xl:justify-between'>
+        <CardTitle asChild>
+          <h3>Calendar</h3>
+        </CardTitle>
+        <div className='flex shrink-0 grow gap-3 xl:justify-end xl:self-center'>
+          {['Day', 'Week', 'Month'].map((label, i) => (
+            <Button
+              key={i}
+              className='grow basis-[100px] sm:grow-0'
+              variant={activeIndexTab === i ? 'primary' : 'gray'}
+              size={'sm'}
+              onClick={() => setActiveIndexTab(i)}
+            >
+              {label}
+            </Button>
+          ))}
+        </div>
+      </CardHeader>
+      <CardContent className='custom-scroll flex grow flex-col gap-6 overflow-x-hidden'>
+        {activeIndexTab === 0 && <DayTab />}
+        {activeIndexTab === 1 && <DayTab />}
+        {activeIndexTab === 2 && <MonthTab />}
+      </CardContent>
+    </Card>
   )
-}
+})
 
 export { CalendarWidget }

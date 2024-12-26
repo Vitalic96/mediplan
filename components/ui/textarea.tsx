bfa09552142cd166ebec039e5b83496261acb341
designindev <2438599@gmail.com>
2024-12-26 18:55:09 +0200

    changed padding

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<
   return (
     <textarea
       className={cn(
-        'flex min-h-[60px] w-full rounded-2xl rounded-br-none rounded-tl-none border border-[#817474]/20 bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-gray-800/20 focus-visible:border-neutral-950 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+        'flex min-h-[60px] w-full rounded-2xl rounded-br-none rounded-tl-none border border-[#817474]/20 bg-transparent p-3 text-base shadow-sm placeholder:text-gray-800/20 focus-visible:border-neutral-950 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 sm:p-5',
         className,
       )}
       ref={ref}
